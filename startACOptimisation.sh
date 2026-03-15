cf8e18d7afaa38174cb473549d0e0d31164b8d3b
Ratibor Vanin <chester.1.80@mail.ru>
2026-03-15 03:30:37 +0300

    edit sh script; add loop for quantum espresso

--- startACOptimisation.sh
+++ startACOptimisation.sh
@@ -2,7 +2,21 @@
 
 python ACOptimization.py
 
-mpirun -np 4 pw.x -inp ACOptimization/in/a_const_000.in > ACOptimization/out/a_const_000.out
-#for i in ACOptimization/in/*.in; do
-#    mpirun -np 4 pw.x -inp "$i" > "ACOptimization/out/$(basename "${i%.in}.out")"
-#done
+#mpirun -np 4 pw.x -inp ACOptimization/in/a_const_000.in > ACOptimization/out/a_const_000.out
+for i in ACOptimization/in/*.in; do
+    filename=$(basename "$i")
+    start_time=$(date '+%Y-%m-%d %H:%M:%S')
+    start_seconds=$(date +%s)
+
+    echo "File: $filename"
+    echo "Start time: $start_time"
+
+    mpirun -np 4 pw.x -inp "$i" > "ACOptimization/out/${filename%.in}.out"
+
+    end_time=$(date '+%Y-%m-%d %H:%M:%S')
+    end_seconds=$(date +%s)
+    duration=$((end_seconds - start_seconds))
+    echo "Finish time: $end_time"
+    echo "Elapsed time: $duration sec"
+    echo ""
+done
