cf49b539887941f7f522fdd6ed0f6cb9db83caad
Ratibor Vanin <chester.1.80@mail.ru>
2026-03-15 03:20:24 +0300

    temp ver of sh script

--- startACOptimisation.sh
+++ startACOptimisation.sh
@@ -2,6 +2,7 @@
 
 python ACOptimization.py
 
-for i in ACOptimization/in/*.in; do
-    mpirun -np 4 pw.x -inp "$i" > "ACOptimization/out/$(basename "${i%.in}.out")"
-done
+mpirun -np 4 pw.x -inp ACOptimization/in/a_const_000.in > ACOptimization/out/a_const_000.out
+#for i in ACOptimization/in/*.in; do
+#    mpirun -np 4 pw.x -inp "$i" > "ACOptimization/out/$(basename "${i%.in}.out")"
+#done
